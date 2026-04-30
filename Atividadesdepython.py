64b0ba5a75f88c54c3573e6231c14cab19ff14c9
alexandrebezerracs <62438285+alexandrebezerracs@users.noreply.github.com>
2026-04-30 08:40:07 -0300

    Update Atividadesdepython.py

--- Atividadesdepython.py
+++ Atividadesdepython.py
@@ -3,8 +3,8 @@
 2. peça a temperatura em fahrenheit e exiba o valor convertido em celsius
 3. peça três números e exiba a media aritimética entre eles.ok
 4. solicite o nome do usuário e mostre qual o tipo de dados esta armazenado ok
-5. crie peça o valor em dolarese exibva o valor correspondente em reais.
-6. crie uma lista com 10 números e exiba a lista com o dobro de cada um dos números
+5. crie peçe o valor m dolares e exiba o valor correspondente em reais.
+6. 
 7. solicite dois números e verifique se o segundo é menor que o primeiro ok
 8. solicite o nome e sobrenome de dois usuários e imprima  o nome do primeiro com o sobrenome do segundo e inversamente ok
 9. peça um número e exiba a metade dele ok
@@ -43,24 +43,44 @@ time.sleep(5)
 os.system("cls")
 
 
+#NOTE - 3
+
+nm1 = float(input("Digite o Primeiro número: "))
+nm2 = float(input("Digite o Segundo número: "))
+nm3 = float(input("Digite o Terceiro número: "))
+
+media = (nm1+nm2+nm3)
+
+print(f"A média aritmética é {media}")
+
+time.sleep(5)
+os.system("cls")
 #NOTE - 4
 
 nomedousuario=input("Digite o seu nome: ")
 print("olá",nomedousuario)
-print("O tipo de dado amarzenado é string")
+print("O tipo de dado amarzenado é",type(nomedousuario))
 
-
-
+time.sleep(5)
+os.system("cls")
 #NOTE - 5
 dolra = float(input("Digite o valor em dolar: "))
 real = dolra*4.98
 print(f"O valor em reais é {real:.2f}")
 
+time.sleep(5)
+os.system("cls")
 #NOTE - 6
 
-listanumeros=["1",'2','3','4','5','6','7','8','9','10']
-print=("o dobro de cada número da lista é ", 1*2, 2*2, 3*2, 4*2, 5*2, 6*2, 7*2, 8*2, 9*2, 10*2,)
+listanumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+for numero in listanumeros:
+    dobro = numero * 2
+    print(f"O dobro de {numero} é {dobro}")
+
+    
+time.sleep(5)
+os.system("cls")
 #NOTE - 7
 
 nummm1=int(input("Digite um número: "))
@@ -71,6 +91,8 @@ if nummm1>nummm2:
 else:
     print("O segundo é maior que o primeiro")
 
+time.sleep(5)
+os.system("cls")
 #NOTE - 8
 
 nome1=input("Digite o nome da 1ª pessoa")
@@ -81,33 +103,46 @@ snome2=input("Digite o sobrenome da 2ª pessoa")
 print(nome1,snome2)
 print("\n",nome2,snome1)
 
+time.sleep(5)
+os.system("cls")
 #NOTE - 9
 
 nuum=int(input("digite um número: "))
 print(f"A metade do número é {nuum/2}")
 
+time.sleep(5)
+os.system("cls")
 #NOTE - 10
 
 nn1=int(input("Digite a altura do retangulo: "))
 nn2=int(input("Digite a largura do retangulo: "))
 area=nn1*nn2
 print(f"A área é {area}")
+
+time.sleep(5)
+os.system("cls")
 #NOTE - 11
 
 nnn1=int(input("Digite um número: "))
 print(f"{nnn1-1}, {nnn1}, {nnn1+1}")
 
+time.sleep(5)
+os.system("cls")
 #NOTE - 12
 
 nj1=int(input("Digite um número: "))
 print(f'{nj1*2}, {nj1*3}, {nj1**1/2}')
       
+time.sleep(5)
+os.system("cls")
 #NOTE - 13
 
 nk1=int(input("Digite um número: "))
 print(f"{nk1**2}")
 
 
+time.sleep(5)
+os.system("cls")
 #NOTE - 14
 
 n1=int(input("Digite o 1º número: "))
@@ -121,7 +156,9 @@ print(f"O produto entre os tres valores é {prd}")
 time.sleep(5)
 os.system ("cls")
 
-# 
+
+time.sleep(5)
+os.system("cls")
 #NOTE - 15
 
 p1=float(input("Digite o valor do produto: ")).replace(",", ".")
@@ -130,6 +167,8 @@ desconto=p1*0.1
 
 print(f"Valor com desconto: {desconto:.2f}")
 
+time.sleep(5)
+os.system("cls")
 #NOTE - 16
 time.sleep(5)
 os.system("cls")
@@ -146,6 +185,8 @@ time.sleep(5)
 os.system("cls")
 
 
+time.sleep(5)
+os.system("cls")
 #NOTE - 17
 
 metros=float("Digite o valor em metros: ")
@@ -156,11 +197,15 @@ print(f"O valor em centimetros é {cent}")
 print(f"O valor em milimetros é {mili}")
 print(f"O valor em kilometros é {kil}")
 
+time.sleep(5)
+os.system("cls")
 #NOTE - 18
 
 h=int(input("Digite as horas: "))
 print(h*60)
 
+time.sleep(5)
+os.system("cls")
 #NOTE - 19
 
 dit=float(input("Digite a distancia percorrida em km: "))
@@ -169,6 +214,8 @@ media=dit/comb
 print(f"A media de comsumo é {media}L/km")
 
 
+time.sleep(5)
+os.system("cls")
 #NOTE - 20
 
 numero=float(input("Digite qualque número real: "))
@@ -177,4 +224,3 @@ print(f"O número absoluto é {numero:.0f}")
 
 time.sleep(5)
 os.system("cls")
-
